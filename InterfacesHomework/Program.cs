f984cb0abf3db50129ddbee84061a35b06715869
Philip Greene <PGreene@spartaglobal.com>
2019-09-03 00:58:31 +0100

    Add project files.

---
+++ InterfacesHomework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Item();
+            var b = new Item();
+            a.Name = "Bob";
+            b.Name = "Carly";
+            Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b)); // a < b returns -1
+            a.Name = "Carly";
+            b.Name = "Carly";
+            Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b)); // a == b returns 0
+            a.Name = "Edward";
+            b.Name = "Carly";
+            Console.WriteLine("{0} compared to {1} is {2}", a.Name, b.Name, a.CompareTo(b)); // a > b returns 1
+        }
+    }
+    public class Item : IComparable
+    {
+        public string Name;
+
+        public int CompareTo(object o)
+        {
+            Item that = o as Item;
+            return this.Name.CompareTo(that.Name);
+        }
+    }
+}
